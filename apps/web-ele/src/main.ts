376c66b743e4e98905d6c626225e65788ed1257e
Mm-Dxx <62460213+Mm-Dxx@users.noreply.github.com>
2025-09-04 16:08:17 +0800

    style：markdown使用github-markdown-css

--- apps/web-ele/src/main.ts
+++ apps/web-ele/src/main.ts
@@ -4,6 +4,7 @@ import { unmountGlobalLoading } from '@vben/utils';
 import { overridesPreferences } from './preferences';
 
 import '#/assets/style/index.scss';
+import 'github-markdown-css/github-markdown.css';
 
 /**
  * 应用初始化完成之后再进行页面加载渲染
