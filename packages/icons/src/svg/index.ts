b635c0bbb33d3abdaa3b04f056a83f3c568c9954
吴泽威 <958142070@qq.com>
2026-02-27 15:04:58 +0800

    refactor: 删除无用 SVG 并更新菜单图标
    - 移除 packages/icons/src/svg/icons/antdv-logo.svg 文件
    - 从索引文件中删除 SvgAntdvLogoIcon 的导入和导出
    - 更新 menu-api.svg 图标，重新设计路径并优化视觉效果
    - 调整 menu-document.svg 图标，更新 SVG 结构和路径属性
    
    (cherry picked from commit 1dfa03a7f35d3e579377f620a99bd0c059dc5322)

--- packages/icons/src/svg/index.ts
+++ packages/icons/src/svg/index.ts
@@ -2,20 +2,11 @@ import { createIconifyIcon } from '@vben-core/icons';
 
 import './load.js';
 
-const SvgAvatar1Icon = createIconifyIcon('svg:avatar-1');
-const SvgAvatar2Icon = createIconifyIcon('svg:avatar-2');
-const SvgAvatar3Icon = createIconifyIcon('svg:avatar-3');
-const SvgAvatar4Icon = createIconifyIcon('svg:avatar-4');
-const SvgDownloadIcon = createIconifyIcon('svg:download');
-const SvgCardIcon = createIconifyIcon('svg:card');
-const SvgBellIcon = createIconifyIcon('svg:bell');
-const SvgCakeIcon = createIconifyIcon('svg:cake');
-const SvgAntdvLogoIcon = createIconifyIcon('svg:antdv-logo');
+
 const SvgCaretRightIcon = createIconifyIcon('svg:caret-right');
 const SvgCloseIcon = createIconifyIcon('svg:close-duotone');
 const SvgCopyIcon = createIconifyIcon('svg:copy');
 const SvgFormatLeftIcon = createIconifyIcon('svg:format-left');
-const SvgExchangeIcon = createIconifyIcon('svg:exchange');
 const SvgApiPrefixIcon = createIconifyIcon('svg:api-prefix');
 const SvgMenuApiIcon = createIconifyIcon('svg:menu-api');
 const SvgMenuDocumentIcon = createIconifyIcon('svg:menu-document');
@@ -24,20 +15,10 @@ const SvgMenuHomeIcon = createIconifyIcon('svg:menu-home');
 const SvgMenuSafetyIcon = createIconifyIcon('svg:menu-safety');
 
 export {
-  SvgAntdvLogoIcon,
   SvgApiPrefixIcon,
-  SvgAvatar1Icon,
-  SvgAvatar2Icon,
-  SvgAvatar3Icon,
-  SvgAvatar4Icon,
-  SvgBellIcon,
-  SvgCakeIcon,
-  SvgCardIcon,
   SvgCaretRightIcon,
   SvgCloseIcon,
   SvgCopyIcon,
-  SvgDownloadIcon,
-  SvgExchangeIcon,
   SvgFormatLeftIcon,
   SvgMenuApiIcon,
   SvgMenuDocumentIcon,
